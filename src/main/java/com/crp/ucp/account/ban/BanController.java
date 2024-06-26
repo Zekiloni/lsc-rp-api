16c82ae846bdd6bb5030f426dde625365dd272cd
Zekiloni <zekirija17@gmail.com>
2024-06-26 22:22:03 +0200

    ban controller and implementation of list bans method (/ban)

---
+++ src/main/java/com/crp/ucp/account/ban/BanController.java
@@ -0,0 +1,27 @@
+package com.crp.ucp.account.ban;
+
+import com.crp.ucp.server.api.BanApi;
+import com.crp.ucp.server.model.Ban;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BanController implements BanApi {
+
+    private final BanService banService;
+    private final BanMapper banMapper;
+
+    @Override
+    public ResponseEntity<List<Ban>> listAccountBans(Integer accountId) {
+        return BanApi.super.listAccountBans(accountId);
+    }
+
+    @Override
+    public ResponseEntity<List<Ban>> listBans() {
+        return ResponseEntity.ok(banMapper.mapTo(banService.getAllBans()));
+    }
+}
