6fc0063dc01fc9fde76b2d56656e3a28c22d5e2a
Zekiloni <zekirija17@gmail.com>
2024-06-26 23:02:05 +0200

    passing role based on admin level to token generation and account id

--- src/main/java/com/crp/ucp/account/authentication/AuthenticationService.java
+++ src/main/java/com/crp/ucp/account/authentication/AuthenticationService.java
@@ -4,6 +4,8 @@ import com.crp.ucp.account.AccountEntity;
 import com.crp.ucp.account.AccountMapper;
 import com.crp.ucp.account.AccountService;
 import com.crp.ucp.account.AccountException;
+import com.crp.ucp.security.JwtService;
+import com.crp.ucp.security.RoleType;
 import com.crp.ucp.server.model.Account;
 import com.crp.ucp.server.model.Authentication;
 import com.crp.ucp.server.model.AuthenticationStatus;
@@ -35,7 +37,8 @@ public class AuthenticationService {
             throw new BadCredentialsException();
         }
 
-        String token = jwtService.generateToken(auth.getUsername());
+        RoleType role = account.getAdmin() != 0 ? RoleType.ADMIN_ROLE : RoleType.USER_ROLE;
+        String token = jwtService.generateToken(auth.getUsername(), account.getId(), role);
 
         AuthenticationStatus authStatus = new AuthenticationStatus();
         authStatus.setAccount(accountMapper.mapTo(account));
