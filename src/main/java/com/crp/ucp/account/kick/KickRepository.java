657d0217a3418e3f775018d62379101542d14c06
Zekiloni <zekirija17@gmail.com>
2024-06-29 00:59:06 +0200

    KickRepository and findByAccountId

---
+++ src/main/java/com/crp/ucp/account/kick/KickRepository.java
@@ -0,0 +1,12 @@
+package com.crp.ucp.account.kick;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface KickRepository extends JpaRepository<KickEntity, Integer> {
+
+    List<KickEntity> findByAccountId(Integer accountId);
+}
