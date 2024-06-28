f7317e756cdf1cdd93e2c8dc85f888612d74d73f
Zekiloni <zekirija17@gmail.com>
2024-06-29 00:59:31 +0200

    add KickEntity

---
+++ src/main/java/com/crp/ucp/account/kick/KickEntity.java
@@ -0,0 +1,40 @@
+package com.crp.ucp.account.kick;
+
+import com.crp.ucp.account.AccountEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "kicks")
+public class KickEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "account_id", referencedColumnName = "id")
+    private AccountEntity account;
+
+    @Size(max = 128)
+    @Column(name = "reason", length = 128)
+    private String reason;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    @JoinColumn(name = "admin_account_id", referencedColumnName = "id")
+    private AccountEntity adminAccount;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+}
