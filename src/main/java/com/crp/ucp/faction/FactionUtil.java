e5e521312a479c79695eeaded18cecc961b12e7b
Zekiloni <zekirija17@gmail.com>
2024-06-29 23:32:03 +0200

    add FactionUtil, throw faction not found exception

---
+++ src/main/java/com/crp/ucp/faction/FactionUtil.java
@@ -0,0 +1,16 @@
+package com.crp.ucp.faction;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.NoSuchElementException;
+import java.util.function.Supplier;
+
+import static java.text.MessageFormat.format;
+
+@UtilityClass
+public class FactionUtil {
+
+    public static Supplier<NoSuchElementException> throwFactionNotFoundException(Integer factionId) {
+        return () -> new NoSuchElementException(format("Faction with ID {0} not found", factionId));
+    }
+}
