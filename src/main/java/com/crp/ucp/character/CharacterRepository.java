94b34e8c9e296c3b3bbb35f7ab55877050b4d542
Zekiloni <zekirija17@gmail.com>
2024-06-29 18:28:06 +0200

    add CharacterRepository.findByFactionId method definition

--- src/main/java/com/crp/ucp/character/CharacterRepository.java
+++ src/main/java/com/crp/ucp/character/CharacterRepository.java
@@ -3,10 +3,13 @@ package com.crp.ucp.character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<CharacterEntity, Integer> {
 
     Optional<CharacterEntity> findByName(String name);
+
+    List<CharacterEntity> findByFactionId(Integer factionId);
 }
