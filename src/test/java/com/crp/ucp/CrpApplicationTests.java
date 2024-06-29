be864c7ecd8ba7433e360d6db6452593b134817a
Zekiloni <zekirija17@gmail.com>
2024-06-29 17:43:08 +0200

    application test properties, using h2 database for context load test, profile test

--- src/test/java/com/crp/ucp/CrpApplicationTests.java
+++ src/test/java/com/crp/ucp/CrpApplicationTests.java
@@ -2,8 +2,10 @@ package com.crp.ucp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CrpApplicationTests {
 
 	@Test
