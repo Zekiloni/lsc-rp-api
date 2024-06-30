ce08fdfd00502e0a8580e4b0ed278cb777bd7fb6
Zekiloni <zekirija17@gmail.com>
2024-06-30 23:03:43 +0200

    add complete build faction member projection

--- src/main/java/com/crp/ucp/faction/FactionService.java
+++ src/main/java/com/crp/ucp/faction/FactionService.java
@@ -3,13 +3,17 @@ package com.crp.ucp.faction;
 import com.crp.ucp.character.CharacterEntity;
 import com.crp.ucp.character.CharacterService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+import static com.crp.ucp.character.CharacterUtil.getCharacterDailyAverageActivity;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FactionService {
 
     private final FactionRepository factionRepository;
@@ -31,12 +35,16 @@ public class FactionService {
     }
 
     private FactionMemberProjection mapTo(CharacterEntity member) {
+        log.info("{} is leader {}", member.getName(), member.getIsLeader());
         return FactionMemberProjection
                 .builder()
+                .accountUsername(member.getAccount().getUsername())
                 .characterId(member.getId())
                 .characterName(member.getName())
+                .characterSkin(member.getSkin())
                 .rankName(member.getRankName())
+                .isLeader(member.getIsLeader() == 1)
+                .averageActivity(getCharacterDailyAverageActivity(member))
                 .build();
     }
-
 }
