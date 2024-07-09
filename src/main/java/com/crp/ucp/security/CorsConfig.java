b93832c0f954aea581c2ee4562e318ca60a77242
Zekiloni <zekirija17@gmail.com>
2024-07-09 23:36:51 +0200

    Add: CORS Config, allowed origins

--- src/main/java/com/crp/ucp/security/CorsConfig.java
+++ src/main/java/com/crp/ucp/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.crp.ucp.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,13 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${lscrp.allowed-origin}")
+    private String lscrpAllowedOrigin;
+
+    private final String devAllowedOrigin = "http://localhost:4200";
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(devAllowedOrigin, lscrpAllowedOrigin)
                         .allowedMethods(
                                 HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
