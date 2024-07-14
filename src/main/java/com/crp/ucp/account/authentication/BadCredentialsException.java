65c5e96495c1dfb4ca58e9b9e66d27b7e160e9c6
Zekiloni <zekirija17@gmail.com>
2024-07-14 21:31:51 +0200

    add translation wrong password

--- src/main/java/com/crp/ucp/account/authentication/BadCredentialsException.java
+++ src/main/java/com/crp/ucp/account/authentication/BadCredentialsException.java
@@ -3,6 +3,6 @@ package com.crp.ucp.account.authentication;
 public class BadCredentialsException extends RuntimeException {
 
     public BadCredentialsException() {
-        super("Wrong password");
+        super("Pogrešna korisnička šifra");
     }
 }
