5b53fef0d338c2d206048bc4cfe26a1b89490420
Zekiloni <zekirija17@gmail.com>
2024-07-20 14:47:17 +0200

    web security config, public routes def, auth provider def

---
+++ src/main/java/com/crp/ucp/security/SecurityUtil.java
@@ -0,0 +1,20 @@
+package com.crp.ucp.security;
+
+import com.crp.ucp.account.RoleType;
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@UtilityClass
+public class SecurityUtil {
+
+    public static SimpleGrantedAuthority createSimpleGrantedAuthority(RoleType role) {
+        return new SimpleGrantedAuthority(role.getValue());
+    }
+
+    public static ArrayList<SimpleGrantedAuthority> createDefaultGrantedAuthority() {
+        return new ArrayList<>(List.of(createSimpleGrantedAuthority(RoleType.USER_ROLE)));
+    }
+}
---
+++ src/main/java/com/crp/ucp/security/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.crp.ucp.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private static final String[] AUTH_WHITELIST = {
+            "/authentication",
+            "/account",
+            "/quiz"
+    };
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
+
