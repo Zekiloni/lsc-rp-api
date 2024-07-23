56161fb4373b4770f9e4f92168a047c76e2369c5
Zekiloni <zekirija17@gmail.com>
2024-07-23 21:14:51 +0200

    secured ban api

--- src/main/java/com/crp/ucp/account/ban/BanController.java
+++ src/main/java/com/crp/ucp/account/ban/BanController.java
@@ -4,6 +4,7 @@ import com.crp.ucp.server.api.BanApi;
 import com.crp.ucp.server.model.Ban;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -16,11 +17,13 @@ public class BanController implements BanApi {
     private final BanMapper banMapper;
 
     @Override
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<List<Ban>> listAccountBans(Integer accountId) {
         return ResponseEntity.ok(banMapper.mapTo(banService.getBansByAccountId(accountId)));
     }
 
     @Override
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<List<Ban>> listBans() {
         return ResponseEntity.ok(banMapper.mapTo(banService.getAllBans()));
     }
