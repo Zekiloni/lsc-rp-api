1feebd6eebc4186c7727718af5990b25d3bfb0f0
Zekiloni <zekirija17@gmail.com>
2024-07-23 21:15:44 +0200

    character mapper, mapping of account id & username

--- src/main/java/com/crp/ucp/character/CharacterMapper.java
+++ src/main/java/com/crp/ucp/character/CharacterMapper.java
@@ -5,12 +5,18 @@ import com.crp.ucp.server.model.Character;
 import com.crp.ucp.server.model.CharacterCreate;
 import com.crp.ucp.vehicle.VehicleMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(uses = {VehicleMapper.class, PropertyMapper.class})
 public interface CharacterMapper {
 
+    @Mappings({
+            @Mapping(source = "account.username", target = "accountUsername"),
+            @Mapping(source = "account.id", target = "accountId"),
+    })
     Character mapTo(CharacterEntity character);
 
     CharacterEntity mapTo(CharacterCreate characterCreate);
