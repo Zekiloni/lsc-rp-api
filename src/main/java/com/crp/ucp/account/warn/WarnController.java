9699d9f24b6b62d4cbe48f518c5f56e32583bc39
Zekiloni <zekirija17@gmail.com>
2024-07-23 21:20:25 +0200

    add warn controller security

--- src/main/java/com/crp/ucp/account/warn/WarnController.java
+++ src/main/java/com/crp/ucp/account/warn/WarnController.java
@@ -4,6 +4,7 @@ import com.crp.ucp.server.api.WarnApi;
 import com.crp.ucp.server.model.Warn;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class WarnController implements WarnApi {
     private final WarnMapper warnMapper;
 
     @Override
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<List<Warn>> listAccountWarns(Integer accountId) {
         return ResponseEntity.ok(warnMapper.mapTo(warnService.getWarnsByAccountId(accountId)));
     }
