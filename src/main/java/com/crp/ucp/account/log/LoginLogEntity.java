babfec987be5dfb0cbac85df6e12562e05e95010
Zekiloni <zekirija17@gmail.com>
2024-07-23 21:18:21 +0200

    add login log audit entity

---
+++ src/main/java/com/crp/ucp/account/log/LoginLogEntity.java
@@ -0,0 +1,40 @@
+package com.crp.ucp.account.log;
+
+import com.crp.ucp.account.AccountEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "login_logs")
+public class LoginLogEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "account_id", referencedColumnName = "id")
+    private AccountEntity account;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "ip_address", nullable = false)
+    private String ipAddress;
+
+    @Column(name = "success")
+    private Boolean success;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+}
