bbcef19ea9f31304dd6aa43b610d8cd492c2f7ed
Zekiloni <zekirija17@gmail.com>
2024-07-23 21:18:07 +0200

    protected list account kicks api

--- src/main/java/com/crp/ucp/account/kick/KickController.java
+++ src/main/java/com/crp/ucp/account/kick/KickController.java
@@ -3,6 +3,7 @@ package com.crp.ucp.account.kick;
 import com.crp.ucp.server.model.Kick;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class KickController implements com.crp.ucp.server.api.KickApi {
     private final KickMapper kickMapper;
 
     @Override
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<List<Kick>> listAccountKicks(Integer accountId) {
         return ResponseEntity.ok(kickMapper.mapTo(kickService.getKicksByAccountId(accountId)));
     }
