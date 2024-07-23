44272c166b3c97fef74c94a1faef493c32f62f7d
Zekiloni <zekirija17@gmail.com>
2024-07-23 21:18:51 +0200

    add LoginLogRepository & find by account id query method

---
+++ src/main/java/com/crp/ucp/account/log/LoginLogRepository.java
@@ -0,0 +1,12 @@
+package com.crp.ucp.account.log;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoginLogRepository extends JpaRepository<LoginLogEntity, Integer> {
+
+    List<LoginLogEntity> findByAccountId(Integer accountId);
+}
