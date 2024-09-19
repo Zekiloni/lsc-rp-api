b8e767a888823552276ffd69a94485e2276499aa
Zekiloni <zekirija17@gmail.com>
2024-09-19 21:41:44 +0200

    add ResetPasswordTokenException model

---
+++ src/main/java/net/lscrp/ucp/account/password/reset/ResetPasswordTokenException.java
@@ -0,0 +1,8 @@
+package net.lscrp.ucp.account.password.reset;
+
+public class ResetPasswordTokenException extends RuntimeException {
+
+    public ResetPasswordTokenException(String message) {
+        super(message);
+    }
+}
